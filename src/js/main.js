615ea0c89166a15bdf61832aed58c37db78beafd
Dylan <dylan.liu237@gmail.com>
2019-06-14 17:16:18 +0800

    add language selector and zone selector

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,3 @@
-import $ from 'jquery';
-// var a = new Date().toLocaleString('en', {timeZone: 'Asia/Taipei',day: '2-digit',month: 'short',year: 'numeric', hour: 'numeric',  minute: 'numeric',  second: 'numeric',hour12: false});
-
-// let zone = ['Europe/London', 'Asia/Taipei', 'Australia/Sydney', 'Asia/Bangkok', 'America/New_york'];
-// let lang = ['en', 'zh-TW'];
-// let getTimeConfig = {
-//     day: '2-digit',
-//     month: 'short',
-//     year: 'numeric',
-//     hour: 'numeric',
-//     minute: 'numeric',
-//     second: 'numeric',
-//     hour12: false
-// };
-
-// this.zone = ['Europe/London', 'Asia/Taipei', 'Australia/Sydney', 'Asia/Bangkok', 'America/New_york'];
-// this.lang = ['en', 'zh-TW'];
-// this.getTimeConfig = {
-//   day: '2-digit',
-//   month: 'short',
-//   year: 'numeric',
-//   hour: 'numeric',
-//   minute: 'numeric',
-//   second: 'numeric',
-//   hour12: false
-// };
-
-
 class TimeZone {
   constructor(lang, area, config = {
     day: '2-digit',
@@ -36,8 +8,8 @@ class TimeZone {
     // second: 'numeric',
     hour12: false  
   }) {
-    this.lang = lang || 'en';
-    this.zone = area || ['Asia/Taipei','Europe/London', 'Australia/Sydney', 'Asia/Bangkok', 'America/New_york'];
+    this.lang = lang || 'en'; // 語言
+    this.zone = area || ['Asia/Taipei','Europe/London', 'Australia/Sydney', 'Asia/Bangkok', 'America/New_york']; // 放在view上渲染的最終資料
     this.getTimeConfig = {
       day: config.day,
       month: config.month,
@@ -46,32 +18,71 @@ class TimeZone {
       minute: config.minute,
       // second: config.second,
       hour12: config.hour12,
-    };
-    this.cache = []; // 暫存預備丟入toLocaleString() API的物件
-    this.timeZoneStringData = []; // 預備渲染到畫面的資料
+    }; // 預設的 toLocaleString() API參數
+    this.supportingZone = ['Asia/Taipei','Europe/London', 'Australia/Sydney', 'Asia/Bangkok', 'America/New_york', 'Pacific/Chatham']; // 預設支援的地區
+    this.userSelectZone = []; // 使用者選取的所有地區
+    this.configAddingUserSelectZone = []; // 將使用者選取的地區資料與this.getTimeConfig合併
+    this.timeStringData = []; // 預備渲染到畫面的資料
+    this.doms = {
+      zoneSelector: document.querySelector('#zoneSelector'),
+      lanSelector: document.querySelector('#langSelector'),
+    }
+    
+    let self = this;
 
-    // 將this.zone地區資料推送進this.getTimeConfig中，並將新資料推送至this.cache
+    this.init();
+    this.renderZoneSelector();
+    this.createTimeZoneView();
+    this.setEventListener();
+    setInterval(function(){
+      self.init();
+      self.createTimeZoneView();
+    }, 1000)
+
+  }
+  // 渲染支援語言
+  renderZoneSelector() {
+    this.supportingZone.forEach((item) => {
+      const option = document.createElement('option');
+      option.setAttribute('value', item);
+      option.innerText = item.replace(/\w+\/(\w+)/, '$1').replace(/\_/g, ` `);
+      zoneSelector.appendChild(option);
+    })    
+  }
+  // 事件監聽
+  setEventListener() {
+    let self = this;
+    // 語言選擇事件監聽;
+    this.doms.lanSelector.addEventListener('change', function(e){
+
+      console.log("Current Language: " + e.target.value);
+      self.lang = e.target.value;
+      self.createTimeZoneView();
+
+    }, false);
+    // 選擇地區事件監聽
+    this.doms.zoneSelector.addEventListener('change', function(e){
+      // 排除重複
+      if(self.userSelectZone.indexOf(e.target.value) == -1) {
+        self.userSelectZone.push(e.target.value);
+      }
+      self.zone = [...self.userSelectZone];
+      self.init();
+      self.createTimeZoneView();
+    }, false);
+  }
+  // 初始化
+  init() {
+    // 將this.zone地區資料推送進this.getTimeConfig合併，並將新資料指向至this.configAddingUserSelectZone
+    this.configAddingUserSelectZone = [];
     this.zone.forEach((item) => {
       let cloneConfig = Object.assign({}, this.getTimeConfig);
       cloneConfig.timeZone = item;
-      this.cache.push(cloneConfig);
+      this.configAddingUserSelectZone.push(cloneConfig);
     })
-
-    // 第一次渲染畫面
-    this.createTimeZoneView();
-    
-    // 每分鐘更新畫面
-    const self = this;
-    setInterval(function(){
-      self.createTimeZoneView();
-    }, 60000)
-
-    console.log(this.cache);
-    console.log(this.timeZoneStringData);
   }
-
-  // 渲染畫面方法
-  createTimeZoneView() {
+  // 渲染時區資料
+    createTimeZoneView() {
     const wrapNode = document.querySelector('.content');
 
     // 每次更新移除子節點重新渲染
@@ -79,33 +90,80 @@ class TimeZone {
       wrapNode.removeChild(wrapNode.firstChild);
     }
 
-    // 將toLocaleString() API產生的字串丟入this.timeZoneStringData陣列中
-    this.timeZoneStringData = this.cache.map((item) => {
+    // 將toLocaleString() API產生的字串丟入this.timeStringData陣列中
+    this.timeStringData = this.configAddingUserSelectZone.map((item) => {
       return new Date().toLocaleString(this.lang, item);
     })
 
     // 將資料塞入子節點，渲染畫面
-    this.timeZoneStringData.forEach((item, index) => {
-      let newItem = item.split(',');
-      console.log(newItem);
+    this.timeStringData.forEach((item, index) => {
+
       const timeZoneParentNode = document.createElement('div');
       timeZoneParentNode.classList.add('content__item');
-      timeZoneParentNode.innerHTML = `
+
+      if (this.lang == 'en') {
+        let newItem = item.split(',');
+
+        timeZoneParentNode.innerHTML = `
         <div class="information">
           <div class="information__title">
-            ${(this.zone[index].replace(/\w+\/(\w+)/, '$1')).toUpperCase()}
+            ${(this.zone[index].replace(/\w+\/(\w+)/, '$1')).replace(/\_/g, ` `).toUpperCase()}
           </div>
           <div class="information__date">
             ${newItem[0]}${newItem[1]}
           </div>
         </div>
         <div class="time">
-        ${newItem[2]}
+          ${newItem[2]}
         </div>
-      `
+        `
+      } else {
+        let newItem = item.split(' ');
+
+        // 翻譯地名
+        
+        const zhTW = this.zone.map((item) => {
+          switch(item) {
+            case 'Asia/Taipei':
+              return '台北';
+              break;
+            case 'Europe/London':
+              return '倫敦';
+              break;
+            case 'Australia/Sydney':
+              return '雪梨';
+              break;
+            case 'Asia/Bangkok':
+              return '曼谷';
+              break;
+            case 'America/New_york':
+              return '紐約';
+              break;
+            case 'Pacific/Chatham':
+              return '查塔姆';
+              break;
+          }
+        })
+
+        timeZoneParentNode.innerHTML = `
+        <div class="information">
+          <div class="information__title">
+            ${zhTW[index]}
+          </div>
+          <div class="information__date">
+            ${newItem[0]}
+          </div>
+        </div>
+        <div class="time">
+          ${newItem[1]}
+        </div>
+        `
+      }
       wrapNode.appendChild(timeZoneParentNode);
     })
   }
+
+  // 語言設定
 }
 
 const TZ = new TimeZone();
