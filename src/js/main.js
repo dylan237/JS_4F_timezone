faa214e7489cafef521338535279986533a4b41c
Dylan <dylan.liu237@gmail.com>
2019-06-19 10:31:17 +0800

    add features -user language storable -timezone selector supported multilanuage -some css amended

--- src/js/main.js
+++ src/js/main.js
@@ -1,57 +1,57 @@
 class TimeZone {
   constructor(lang, config = {
-    day: '2-digit',
-    month: 'short',
+    hour12: false,
     year: 'numeric',
+    month: 'short',
+    day: '2-digit',
     hour: 'numeric',
     minute: 'numeric',
     // second: 'numeric',
-    hour12: false
   }) {
-    this.lang = lang || 'en'; // 語言
-    this.supportingLang = ['en', 'zh-TW', 'ja-JP-u-ca-japanese', 'ko-KR'];
-    this.getTimeConfig = config, // 預設的 toLocaleString() API參數
-    this.supportingZone = this.getLocalStorage('userSelectedData', 'supportingZoneData')[1] || ['Europe/London', 'Australia/Sydney', 'Asia/Bangkok', 'America/New_york', 'Pacific/Chatham']; // 預設支援的地區
-    this.userSelectZone = this.getLocalStorage('userSelectedData', 'supportingZoneData')[0] || ['Asia/Taipei']; // 使用者選取的所有地區，預設為台北時間
-    this.configAddingUserSelectZone = []; // 將使用者選取的地區資料與this.getTimeConfig合併
-    this.timeStringData = []; // 預備渲染到畫面的資料
+    this.localStorageCaptions = ['userSelectedData', 'supportedTimezoneData', 'userLangData'];
+    this.supportedLanguages = ['en', 'zh-TW', 'ja-JP-u-ca-japanese', 'ko-KR']; // #langSelector選單資料
+    this.supportedTimezone = this.getLocalStorage(this.localStorageCaptions)[1] || ['Europe/London', 'Australia/Sydney', 'Asia/Bangkok', 'America/New_york', 'Pacific/Chatham']; // #userSelectedTimezone選單資料
+    this.userSelectedLang = this.getLocalStorage(this.localStorageCaptions)[2] || lang || 'en'; // 使用者選擇語言
+    this.userSelectedTimezone = this.getLocalStorage(this.localStorageCaptions)[0] || ['Asia/Taipei']; // 使用者選取的所有地區，預設為台北時間
+    this.toLocalStringConfig = config, // 預設的 toLocaleString() API參數
+    this.dataCombine = []; // 將this.userSelectedTimezone與this.toLocalStringConfig合併
+    this.timeStringData = []; // 由this.dataCombine透過toLocaleString() API所產生的資料陣列，為最後渲染到畫面的時間資料
     this.doms = {
       zoneSelector: document.querySelector('#zoneSelector'),
-      lanSelector: document.querySelector('#langSelector'),
+      langSelector: document.querySelector('#langSelector'),
     }
 
-/////////////
+//-------------------------------------------
 
     this.init();
     this.renderLangSelector()
-    this.renderZoneSelectors();
-    this.createTimeZoneView();
+    this.renderZoneSelector();
+    this.renderTimezone();
     this.setEventListener();
     let self = this;
     setInterval(function(){
       self.init();
-      self.createTimeZoneView();
+      self.renderTimezone();
     }, 60000); 
   }
 
-/////////////
-// methods //
-/////////////
+//-------------------------------------------
+// methods 
+//-------------------------------------------
 
   // 渲染支援地區
-  renderZoneSelectors() {
-
-    // 支援地區
+  renderZoneSelector() {
     while(this.doms.zoneSelector.hasChildNodes()) {
       this.doms.zoneSelector.removeChild(this.doms.zoneSelector.firstChild);
     }
     
-    this.supportingZone.forEach((item) => {
+    const self = this;
+    this.supportedTimezone.forEach((item) => {
       const option = document.createElement('option');
       option.setAttribute('value', item);
       // option.setAttribute('data-zone', item);
       option.dataset.zone = item;
-      option.innerText = item.replace(/\w+\/(\w+)/, '$1').replace(/\_/g, ` `);
+      option.innerText = self.multilanguage(item);
       this.doms.zoneSelector.appendChild(option);
     })
     const head = document.createElement('option');
@@ -59,11 +59,19 @@ class TimeZone {
     head.innerText = `-------`
     this.doms.zoneSelector.prepend(head);
   }
+
+  // 渲染支援語言
   renderLangSelector() {
-    // 支援語言
-    this.supportingLang.forEach((item) => {
+    // 將記錄在LocalStorage的使用者選取語言放在第一個
+    let currentLangPrepend = [];
+    this.supportedLanguages.forEach((item) => {
+        item === this.userSelectedLang ? currentLangPrepend.unshift(item) : currentLangPrepend.push(item);
+    })
+
+    currentLangPrepend.forEach((item) => {
       const option = document.createElement('option');
       option.setAttribute('value', item);
+
       if(item === 'en') {
         item = 'English';
       } else if (item === 'zh-TW') {
@@ -73,20 +81,22 @@ class TimeZone {
       } else if (item === 'ko-KR') {
         item = '한국어';
       }
-      
+
       option.innerText = item;
-      this.doms.lanSelector.appendChild(option);
+      this.doms.langSelector.appendChild(option);
     })
   }
-  // 非動態事件監聽
+  // 非動態資料的事件監聽
   setEventListener() {
     let self = this;
 
     // 語言選擇事件監聽;
-    this.doms.lanSelector.addEventListener('change', function(e){
+    this.doms.langSelector.addEventListener('change', function(e){
       if (console) console.log("Current Language: " + e.target.value);
-      self.lang = e.target.value;
-      self.createTimeZoneView();
+      self.userSelectedLang = e.target.value;
+      self.setLocalStorage(self.userSelectedTimezone, self.supportedTimezone, self.userSelectedLang);
+      self.renderTimezone();
+      self.renderZoneSelector();
     }, false);
 
     // 選擇地區事件監聽
@@ -95,32 +105,33 @@ class TimeZone {
       if(e.target.value == 'head') return;
 
       // 處理下拉選單資料 & 和使用者選擇地區資料
-      self.supportingZone = self.supportingZone.filter((item) => {
+      self.supportedTimezone = self.supportedTimezone.filter((item) => {
         return item !== e.target.value;
       })
-      if(self.userSelectZone.indexOf(e.target.value) == -1) {
-        self.userSelectZone.push(e.target.value);
+      if(self.userSelectedTimezone.indexOf(e.target.value) == -1) {
+        self.userSelectedTimezone.push(e.target.value);
       }
 
-      self.setLocalStorage(self.userSelectZone, self.supportingZone);
+      self.setLocalStorage(self.userSelectedTimezone, self.supportedTimezone, self.userSelectedLang);
       self.init();
-      self.createTimeZoneView();
-      self.renderZoneSelectors();
+      self.renderTimezone();
+      self.renderZoneSelector();
     }, false);
-    
   }
   // 初始化
   init() {
-    // 將this.zone地區資料推送進this.getTimeConfig合併，並將新資料指向至this.configAddingUserSelectZone
-    this.configAddingUserSelectZone = [];
-    this.userSelectZone.forEach((item) => {
-      let cloneConfig = Object.assign({}, this.getTimeConfig);
+    console.log(this.userSelectedTimezone);
+    
+    // 將this.zone地區資料推送進this.toLocalStringConfig合併，並將新資料指向至this.dataCombine
+    this.dataCombine = [];
+    this.userSelectedTimezone.forEach((item) => {
+      let cloneConfig = Object.assign({}, this.toLocalStringConfig);
       cloneConfig.timeZone = item;
-      this.configAddingUserSelectZone.push(cloneConfig);
+      this.dataCombine.push(cloneConfig);
     })
   }
-  // 時區資料和doms渲染
-  createTimeZoneView() {
+  // 時區資料和DOM渲染
+  renderTimezone() {
     const wrapNode = document.querySelector('.content');
 
     // 每次更新移除子節點重新渲染
@@ -129,8 +140,8 @@ class TimeZone {
     }
 
     // 將toLocaleString() API產生的字串丟入this.timeStringData陣列中
-    this.timeStringData = this.configAddingUserSelectZone.map((item) => {
-      return new Date().toLocaleString(this.lang, item);
+    this.timeStringData = this.dataCombine.map((item) => {
+      return new Date().toLocaleString(this.userSelectedLang, item);
     })
     
     // 將資料塞入子節點，渲染畫面
@@ -139,13 +150,13 @@ class TimeZone {
       const timeZoneParentNode = document.createElement('div');
       timeZoneParentNode.classList.add('content__item');
 
-      if (this.lang == 'en') {
+      if (this.userSelectedLang == 'en') {
         let newItem = item.split(',');
 
         timeZoneParentNode.innerHTML = `
         <div class="information">
           <div class="information__title">
-            ${(this.userSelectZone[index].replace(/\w+\/(\w+)/, '$1')).replace(/\_/g, ` `).toUpperCase()}
+            ${(this.userSelectedTimezone[index].replace(/\w+\/(\w+)/, '$1')).replace(/\_/g, ` `).toUpperCase()}
           </div>
           <div class="information__date">
             ${newItem[0]}${newItem[1]}
@@ -154,33 +165,14 @@ class TimeZone {
         <div class="time">
           ${newItem[2]}
         </div>
-        <div class="delete" data-zone="${this.userSelectZone[index]}"></div>
+        <div class="delete" data-zone="${this.userSelectedTimezone[index]}"></div>
         `
-      } else if(this.lang == 'ko-KR') {
+      } else if(this.userSelectedLang == 'ko-KR') {
         let newItem = item.split(' ');
 
         // 翻譯地名
-        const KR = this.userSelectZone.map((item) => {
-          switch(item) {
-            case 'Asia/Taipei':
-              return '타이페이';
-              break;
-            case 'Europe/London':
-              return '런던';
-              break;
-            case 'Australia/Sydney':
-              return '시드니';
-              break;
-            case 'Asia/Bangkok':
-              return '방콕';
-              break;
-            case 'America/New_york':
-              return '뉴욕';
-              break;
-            case 'Pacific/Chatham':
-              return '채텀';
-              break;
-          }
+        const KR = this.userSelectedTimezone.map((item) =>{
+          return this.multilanguage(item);
         })
 
         timeZoneParentNode.innerHTML = `
@@ -195,33 +187,14 @@ class TimeZone {
         <div class="time">
           ${newItem[3]}
         </div>
-        <div class="delete" data-zone="${this.userSelectZone[index]}"></div>
+        <div class="delete" data-zone="${this.userSelectedTimezone[index]}"></div>
         `
-      } else if(this.lang == 'zh-TW') {
+      } else if (this.userSelectedLang == 'zh-TW') {
         let newItem = item.split(' ');
 
-                // 翻譯地名
-        const KR = this.userSelectZone.map((item) => {
-          switch(item) {
-            case 'Asia/Taipei':
-              return '台北';
-              break;
-            case 'Europe/London':
-              return '倫敦';
-              break;
-            case 'Australia/Sydney':
-              return '雪梨';
-              break;
-            case 'Asia/Bangkok':
-              return '曼谷';
-              break;
-            case 'America/New_york':
-              return '紐約';
-              break;
-            case 'Pacific/Chatham':
-              return '查塔姆';
-              break;
-          }
+        // 翻譯地名
+        const KR = this.userSelectedTimezone.map((item) =>{
+          return this.multilanguage(item);
         })
 
         timeZoneParentNode.innerHTML = `
@@ -236,33 +209,14 @@ class TimeZone {
         <div class="time">
           ${newItem[1]}
         </div>
-        <div class="delete" data-zone="${this.userSelectZone[index]}"></div>
+        <div class="delete" data-zone="${this.userSelectedTimezone[index]}"></div>
         `
-      } else if (this.lang == 'ja-JP-u-ca-japanese') {
+      } else if (this.userSelectedLang == 'ja-JP-u-ca-japanese') {
         let newItem = item.split(' ');
 
         // 翻譯地名
-        const JP = this.userSelectZone.map((item) => {
-          switch(item) {
-            case 'Asia/Taipei':
-              return '台北';
-              break;
-            case 'Europe/London':
-              return 'ロンドン';
-              break;
-            case 'Australia/Sydney':
-              return 'シドニー';
-              break;
-            case 'Asia/Bangkok':
-              return 'バンコク';
-              break;
-            case 'America/New_york':
-              return 'ニューヨーク';
-              break;
-            case 'Pacific/Chatham':
-              return 'チャタム';
-              break;
-          }
+        const JP = this.userSelectedTimezone.map((item) =>{
+          return this.multilanguage(item);
         })
 
         timeZoneParentNode.innerHTML = `
@@ -277,48 +231,122 @@ class TimeZone {
         <div class="time">
           ${newItem[1]}
         </div>
-        <div class="delete" data-zone="${this.userSelectZone[index]}"></div>
-        `        
+        <div class="delete" data-zone="${this.userSelectedTimezone[index]}"></div>
+        `
       }
       wrapNode.appendChild(timeZoneParentNode);
-      this.deleteBtnListener();
+      this.deleteButtonsListener();
     })
   }
   // 刪除時區事件監聽
-  deleteBtnListener() {
+  deleteButtonsListener() {
     const deleteBtn = document.querySelectorAll('.delete');
     const self = this;
     
     deleteBtn.forEach((item) => {
       item.addEventListener('click', function(e){
         // 只剩一筆資料時禁止刪除
-        if(self.userSelectZone.length === 1) return;
+        if(self.userSelectedTimezone.length === 1) return;
 
         // 處理下拉選單資料 & 和使用者選擇地區資料
-        self.userSelectZone = self.userSelectZone.filter((item) => {
+        self.userSelectedTimezone = self.userSelectedTimezone.filter((item) => {
           return item !== e.target.dataset.zone
         })
-        if(self.supportingZone.indexOf(e.target.dataset.zone)== -1) {
-          self.supportingZone.push(e.target.dataset.zone);
+        if(self.supportedTimezone.indexOf(e.target.dataset.zone)== -1) {
+          self.supportedTimezone.push(e.target.dataset.zone);
         }
 
-        self.setLocalStorage(self.userSelectZone, self.supportingZone);
+        self.setLocalStorage(self.userSelectedTimezone, self.supportedTimezone, self.userSelectedLang);
         self.init();
-        self.createTimeZoneView();
-        self.renderZoneSelectors();
+        self.renderTimezone();
+        self.renderZoneSelector();
       }, false);
     })
   }
-  setLocalStorage(userSelectZone, supportingZone) {
-    const userSelectZoneData = JSON.stringify(userSelectZone);
-    const supportingZoneData = JSON.stringify(supportingZone);
-    localStorage.setItem('userSelectedData', userSelectZoneData);
-    localStorage.setItem('supportingZoneData', supportingZoneData);
+  // 多國語系
+  multilanguage(item) {
+    if(this.userSelectedLang === 'en') {
+      return item.replace(/\w+\/(\w+)/, '$1').replace(/\_/g, ` `);
+
+    } else if (this.userSelectedLang === 'zh-TW') {
+      switch(item) {
+        case 'Asia/Taipei':
+          return '台北';
+          break;
+        case 'Europe/London':
+          return '倫敦';
+          break;
+        case 'Australia/Sydney':
+          return '雪梨';
+          break;
+        case 'Asia/Bangkok':
+          return '曼谷';
+          break;
+        case 'America/New_york':
+          return '紐約';
+          break;
+        case 'Pacific/Chatham':
+          return '查塔姆';
+          break;
+      }
+    } else if(this.userSelectedLang === 'ja-JP-u-ca-japanese') {
+      switch(item) {
+        case 'Asia/Taipei':
+          return '台北';
+          break;
+        case 'Europe/London':
+          return 'ロンドン';
+          break;
+        case 'Australia/Sydney':
+          return 'シドニー';
+          break;
+        case 'Asia/Bangkok':
+          return 'バンコク';
+          break;
+        case 'America/New_york':
+          return 'ニューヨーク';
+          break;
+        case 'Pacific/Chatham':
+          return 'チャタム';
+          break;
+      }
+    } else if(this.userSelectedLang === 'ko-KR') {
+      switch(item) {
+        case 'Asia/Taipei':
+          return '타이페이';
+          break;
+        case 'Europe/London':
+          return '런던';
+          break;
+        case 'Australia/Sydney':
+          return '시드니';
+          break;
+        case 'Asia/Bangkok':
+          return '방콕';
+          break;
+        case 'America/New_york':
+          return '뉴욕';
+          break;
+        case 'Pacific/Chatham':
+          return '채텀';
+          break;
+      }
+    }
   }
-  getLocalStorage(LStitle1, LStitle2) {
-    const userSelectZoneData = JSON.parse(localStorage.getItem(LStitle1));
-    const supportingZoneData = JSON.parse(localStorage.getItem(LStitle2));
-    return [userSelectZoneData, supportingZoneData];
+  // LocalStorage
+  setLocalStorage(userSelectedTimezone, supportedTimezone, userLang) {
+    const userSelectedTimezoneData = JSON.stringify(userSelectedTimezone);
+    const supportedTimezoneData = JSON.stringify(supportedTimezone);
+    const userLangData = JSON.stringify(userLang);
+    localStorage.setItem('userSelectedData', userSelectedTimezoneData);
+    localStorage.setItem('supportedTimezoneData', supportedTimezoneData);
+    localStorage.setItem('userLangData', userLangData);
+  }
+  getLocalStorage(data) {
+    const userSelectedTimezoneData = JSON.parse(localStorage.getItem(data[0]));
+    const supportedTimezoneData = JSON.parse(localStorage.getItem(data[1]));
+    const langData = JSON.parse(localStorage.getItem(data[2]));
+    return [userSelectedTimezoneData, supportedTimezoneData, langData];
   }
 }
 
